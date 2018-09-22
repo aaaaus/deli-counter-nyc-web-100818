23459a83e6de60af343faa2306d33c18228daf88
Austin Luft <ausluft@gmail.com>
2018-09-22 02:46:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length < 1
+    "The line is currently empty."
+  else
+end
+
+def take_a_number(array, string)
+end
+
+def now_serving(array)
+  if array.length < 1
+    puts "There is nobody waiting in line!"
+  else
+    puts array.pop
+  end
